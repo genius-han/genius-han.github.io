b4cbfcfd4a29cda1883a84815fb76e71a8b0a9a4
genius-han <52880407+genius-han@users.noreply.github.com>
2019-07-15 02:18:54 +0900

    Delete makerss.sh

--- makerss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-PATH_MAKERSS=/home/pi/User/git/genius-han.github.io
-PATH_GIT=/home/pi/User/git/genius-han.github.io
-
-cd $PATH_MAKERSS
-python makerss_main.py
-mv $PATH_MAKERSS/*.xml $PATH_GIT
-$PATH_GIT/commit.sh
-
