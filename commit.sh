9afc5858eb2388a19cd42604171a917d3971938a
genius-han <gooddayh@gmail.com>
2019-07-23 00:13:35 +0900

    20190723-0013

--- commit.sh
+++ commit.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 export LANG=en_US.utf8
+PATH_FLEXGET=/home/pi/User/flexget/
 NowDate=$(date +%Y%m%d)-$(date +%H%M)
 
 cd /home/pi/User/git/genius-han.github.io/
 git add *
 git commit -m $NowDate
 git push
+sudo $PATH_FLEXGET/flexget.sh
+
+
